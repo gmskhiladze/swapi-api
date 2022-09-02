2e3a8cb1b4eabfabc5348cddbc07abe7d987ceb1
Giorgi Mskhiladze <g.mskhiladze99@gmail.com>
2022-09-02 18:54:38 +0400

    fetch movie data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.scss';
+import axios from "axios";
+import {useState} from "react";
+import MovieList from "./components/MovieList/MovieList";
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [movieList, setMovieList] = useState([]);
+
+  const fetchMoviesHandler =  async () => {
+      const response = await axios.get('https://swapi.dev/api/films/');
+
+      if (response.status === 200){
+          const data = await response.data;
+          setMovieList((prevState) =>
+              ( [{
+                  ...prevState,
+                  results: data
+              }])
+          )
+          console.log(data)
+      }else{
+          throw new Error(`Error: Status Code - ${response.status}`)
+      }
+      setTimeout( ()=>{
+          console.log(`thia is a ${movieList}`)
+
+      }, 1000)
+
+  }
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+
+      <section>
+        <MovieList movies={movieList}/>
+      </section>
+    </>
   );
 }
 
---
+++ src/components/MovieList/MovieList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MovieList(movieList) {
+    return (
+        <ul>
+            {/*{movieList.movies.map((movie) => (*/}
+            {/*    // <Movie*/}
+            {/*    //     key={movie.id}*/}
+            {/*    //     title={movie.title}*/}
+            {/*    //     releaseDate={movie.releaseDate}*/}
+            {/*    //     openingText={movie.openingText}*/}
+            {/*    // />*/}
+            {/*))}*/}
+        </ul>
+    );
+}
+
+export default MovieList;
